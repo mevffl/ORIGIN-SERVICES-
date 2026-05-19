18a0e9db8a61cfc06bb2da977207c13362ee66cb
mevffl <mevfflwcv@gmail.com>
2026-05-19 16:14:13 +0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,28 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttpModule from "pino-http";
+
 import router from "./routes";
 import { logger } from "./lib/logger";
+
+const pinoHttp = (pinoHttpModule as any).default || pinoHttpModule;
 
 const app: Express = express();
 
 app.use(
   pinoHttp({
     logger,
+
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +30,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
